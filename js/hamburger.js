a0f30011f62a52d6b5f48c8da62d92eb238c5820
Your NameBGAmaster <scanforall@mail.ru>
2021-09-21 18:45:15 +0300

    fix gb menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,4 +1,4 @@
-document.querySelector('.hamburger').onclick = function(){
-  document.querySelector('.hamburger-icon').classList.toggle('hamburger-icon-active');
-  document.querySelector('.menu-hidden__list').classList.toggle('menu-hidden__list-active');
+function burgerIcon(icon) {
+  icon.classList.toggle("change");
+  document.querySelector('.gb__list').classList.toggle('hidden');
 }
